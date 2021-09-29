d7957dcb90124f0658092d2623503c78842e3bf1
mathdude3482 <orajsamej@gmail.com>
2021-09-29 00:20:18 -0400

    Updated the code.

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -15,34 +15,72 @@ import java.util.Scanner;
 public class Solution34 {
     //Create an ArrayList that contains a list of names.
     private static ArrayList <String> name = new ArrayList<>();
+    private static final Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         //add names to the ArrayList.
-        name.add("");
+        name.add(0,"John Smith");
+        name.add(1,"Jackie Jackson");
+        name.add(2,"Chris Jones");
+        name.add(3,"Amanda Cullen");
+        name.add(4,"Jeremy Goodwin");
         //Call a getSize method that will get the size of the list.
-        int size = getSize();
+        int size = getSize(name);
         //Print out the initial list, with each name on its own line. Use a for loop here.
-        System.out.println(displayName());
+        System.out.println("There are " + size + " names:");
+        for(int i = 0; i < size; i++)
+            System.out.println((name.get(i)));
+        //empty print statements to match the format of the output.
+        System.out.println();
         //call a removeName method that will remove a name from the list.
-        ArrayList<String> newlist = new ArrayList<>();
-        newlist = removeName(name);
-        //Print out the revised list, with each name on its own line. Use a for loop here.
-        System.out.println(displayName());
+        String remove = takeOutName("Enter an employee name to remove:");
+        System.out.println();
+        //declare a new list called newlist that will be used to store the new list of names
+        // after removal of a name.
+        ArrayList <String> newlist = removeName(name, remove);
+        //get the size of newList.
+        int newsize = getSize(newlist);
+        if(newsize == size)
+        {
+            //if the size is the same, do not print out the list again.
+            System.out.println("No changes have been made. The list is the same as before.");
+        }
+        else
+        {
+            //Print out the newList, with each name on its own line. Use a for loop here.
+            System.out.println("There are " + newsize + " names:");
+            for (int i = 0; i < newsize; i++)
+                {
+                System.out.println(newlist.get(i));
+                }
+        }
     }
-    private static String displayName() {
-        //take in one parameter: the ArrayList.
-        //print out the names.
-        return "";
-    }
-
-    private static int getSize() {
+    public static int getSize(ArrayList<String> names) {
         //take in one parameter: the list of names.
         //return the size of the array.
-        return 0;
+        return names.size();
     }
-
-    private static ArrayList<String> removeName(ArrayList<String> names) {
-        //take in one parameter: the list of names.
-        //remove an element from the array.
+    private static String takeOutName(String prompt) {
+        //take in one parameter: the prompt.
+        //print out the prompt.
+        System.out.print(prompt);
+        //return the user input.
+        return in.nextLine();
+    }
+    //take in two parameters: the list of names and the name the user wants removed.
+    public static ArrayList<String> removeName(ArrayList<String> names, String removeName) {
+        //remove any whitespace from the user input.
+        String newremoveName = removeName.replaceAll("\\s", "");
+        //if the name exists, remove the name from the arrayList.
+        for (int i = 0; i < names.size(); i++){
+            //remove whitespace from each name in the arrayList.
+            String newName = names.get(i).replaceAll("\\s", "");
+            if (newName.equalsIgnoreCase(newremoveName))
+            {
+                names.remove(i);
+                return names;
+            }
+        }
+        //otherwise, keep the list as it was before.
         //return the array.
         return names;
     }
