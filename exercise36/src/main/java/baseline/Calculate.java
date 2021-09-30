adadf2df510c8a593459990bea9daec7a8832cb6
mathdude3482 <orajsamej@gmail.com>
2021-09-30 03:18:46 -0400

    Updated the code.

--- exercise36/src/main/java/baseline/Calculate.java
+++ exercise36/src/main/java/baseline/Calculate.java
@@ -1,25 +1,65 @@
 package baseline;
+import java.util.List;
 
 public class Calculate {
+    private static double sum;
+    //hide the implicit public constructor.
+    private Calculate(){
+    }
     //this class will house all the calculation functions needed.
-    public static double getAverage(){
+    public static double getAverage(List<Double> numbers){
         //take in one parameter: the ArrayList of Doubles.
         //return the average of the elements.
-        return 0;
+        for (int i = 0; i < numbers.size(); i++){
+           sum = sum + numbers.get(i);
+        }
+        return sum / numbers.size();
     }
-    public static double getMinimum(){
+    public static double getMinimum(List<Double> numbers){
         //take in one parameter: the ArrayList of Doubles.
         //return the smallest value in the ArrayList.
-        return 0;
+        Double minimum = Double.MAX_VALUE;
+        //check through each element of the list.
+        for (Double i: numbers)
+        {
+            //if minimum is greater, change minimum to be i.
+            if (minimum > i) {
+                minimum = i;
+            }
+        }
+        return minimum;
     }
-    public static double getMaximum(){
+    public static double getMaximum(List<Double> newList){
         //take in one parameter: the ArrayList of Doubles.
         //return the largest value in the ArrayList.
-        return 0;
+        Double maximum = Double.MIN_VALUE;
+        //check through each element of the list.
+        for (Double i: newList)
+        {
+            //if maximum is smaller, change maximum to be i.
+            if (maximum < i) {
+                maximum = i;
+            }
+        }
+        return maximum;
     }
-    public static double getstDeviation(){
+    public static double getstDeviation(List<Double> newList, double average){
         //take in one parameter: the ArrayList of Doubles.
         //return the standard deviation of the elements.
-        return 0;
+        Double mean = average;
+        double sum = 0;
+        for (int i = 0; i < newList.size(); i++)
+        {
+            double val = newList.get(i);
+
+            // Step 2:
+            double squrDiffToMean = Math.pow(val - mean, 2);
+
+            // Step 3:
+            sum += squrDiffToMean;
+        }
+
+        double meanOfDiffs = sum / (newList.size());
+        return Math.sqrt(meanOfDiffs);
     }
 }
--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -1,63 +1,126 @@
 package baseline;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 James Jaro
  */
 //Write a program that prompts for response times from a website in milliseconds.
-// It should keep asking for values until the user enters “done.”
-//The program should print the average time (mean), the minimum time,
+// It should keep asking for values until the user enters done.
+//The program should print the average time, the minimum time,
 // the maximum time, and the population standard deviation.
 //Create functions called average, max, min, and std,
 // which take in a list of numbers and return the results.
 //Use loops and arrays to perform the input and mathematical operations.
-//Be sure to exclude the "done" entry from the inputs.
+//Be sure to exclude the done entry from the inputs.
 //Be sure to properly ignore any invalid inputs.
 //Keep the input separate from the processing and the output.
 public class Solution36 {
-    public static final Scanner in = new Scanner(System.in);
-    public static final ArrayList<String> numbers = new ArrayList<>();
+    private static final Scanner in = new Scanner(System.in);
+    private static final ArrayList<String> numbers = new ArrayList<>();
+    private static double average;
+    private static double min;
+    private static double max;
+    private static double stdev;
     public static void main(String[] args) {
         //Ask the user for a number. Do this in a do-while loop.
         //enter the do-while loop here.
-        String data = getData();
-        //Check if the user input is valid.
-        boolean valid = checkData();
-        //If user input is invalid, do not store the string into the array.
-        //Ask the user again for correct values.
-        //Otherwise, store the inputs into the array.
-        //If the user input is done, exit the loop.
+        String data = "";
+        boolean controlLoop = true;
+        do{
+            data = getData("Enter a number: ");
+            boolean valid = checkData(data);
+            if (!valid){
+                //if the input is not digits or the word done, print out this message.
+                System.out.println("Incorrect data. Enter again.");
+            }
+            else
+            {
+                //add the numbers into the array.
+                numbers.add(data);
+            }
+            //if the String passed in is done:
+            if (data.equalsIgnoreCase("done")){
+                //remove the word done from the array.
+                numbers.remove(data);
+                //end the loop.
+                controlLoop = false;
+            }
+        }while(controlLoop);
         //call the convertData function to convert the Strings into doubles.
-        ArrayList <Double> newList = convertData();
-        //Call the functions of the Calculate class (functions listed above)
+        ArrayList <Double> newList = convertData(numbers);
+        System.out.print("The numbers are: ");
+        for(int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i) + ", ");
+        }
+
+        //Call the functions of the Calculate class
         //and store them into either integers or doubles.
-        double average = Calculate.getAverage();
-        double min = Calculate.getMinimum();
-        double max = Calculate.getMaximum();
-        double stdev = Calculate.getstDeviation();
+        if(numbers.size() != 0)
+        {
+            average = Calculate.getAverage(newList);
+            min = Calculate.getMinimum(newList);
+            max = Calculate.getMaximum(newList);
+            stdev = Calculate.getstDeviation(newList, average);
+            System.out.println();
+            final String REPEAT = "%s%.2f%n";
+            System.out.printf(REPEAT,"The average is ", average);
+            System.out.printf(REPEAT,"The minimum is ", min);
+            System.out.printf(REPEAT,"The maximum is ", max);
+            System.out.printf(REPEAT,"The standard deviation is ", stdev);
+        }
         //display the results.
     }
-
-    private static String getData() {
-        //take in one parameter: the prompt.
+    //take in one parameter: the prompt.
+    private static String getData(String prompt) {
         //print out the prompt.
+        System.out.print(prompt);
         //return the user input.
-        return "";
+        return in.nextLine();
     }
-    private static boolean checkData() {
-        //take in one parameter: the user input.
+    //take in one parameter: the user input.
+    private static boolean checkData(String data) {
         //check if the user input is either a number or the word done.
-        //if this is the case, return true.
-        //otherwise, return false.
-        return true;
+        // Make sure to account for whitespace.
+        boolean determine = true;
+        String revisedData = data.replaceAll("\\s", "");
+        //check if the String has any non-digits.
+        String checkString = "\\D+";
+        String decimal = "\\.";
+        Pattern digitPattern = Pattern.compile(checkString);
+        Pattern findDecimal = Pattern.compile(decimal);
+        Matcher checkData = digitPattern.matcher(revisedData);
+        Matcher checkData2 = findDecimal.matcher(revisedData);
+        //if the user input is not done or a non-digitm return false.
+        if (!(revisedData.equalsIgnoreCase("done") ||!checkData.find() || checkData2.find())){
+            determine = false;
+            return determine;
+        }
+        //otherwise, return true.
+        return determine;
     }
-    private static ArrayList convertData() {
-        //take in one parameter: the ArrayList of Strings.
+    //take in one parameter: the ArrayList of Strings.
+    private static ArrayList <Double> convertData(ArrayList <String> number) {
         //use a for loop here.
         //convert each element into a double.
+        ArrayList<Double> newList = new ArrayList<>();
+        try {
+            for (int i = 0; i < number.size(); i++) {
+                double convert = Double.parseDouble(number.get(i));
+                newList.add(i, convert);
+            }
+        }
+        catch(NumberFormatException e){
+            //if the input somehow cannot convert into a double, catch it and kill the program.
+            System.out.println("I'm ending the program if you somehow got this message.");
+        }
         //store these doubles into an ArrayList of doubles.
         //return the new ArrayList.
-        return numbers;
+       return newList;
     }
 }
