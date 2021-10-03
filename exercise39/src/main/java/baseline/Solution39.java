a81bcdd53c16e402983c93208c04cac7486e4510
mathdude3482 <orajsamej@gmail.com>
2021-10-02 22:28:27 -0400

    updated the code.

--- exercise39/src/main/java/baseline/Solution39.java
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -16,19 +16,44 @@ public class Solution39 {
     private static Solution39 app = new Solution39();
     private static Map<String, ArrayList<String>> firstMap = new HashMap<>();
     public static void main(String[] args) {
-        //Create arrays for each entry. Include in each array:
-        // the first name, the position, and the separation date.
+        //Create lists for each entry.
         ArrayList<String> entry1 = new ArrayList<>();
         ArrayList<String> entry2 = new ArrayList<>();
         ArrayList<String> entry3 = new ArrayList<>();
         ArrayList<String> entry4 = new ArrayList<>();
         ArrayList<String> entry5 = new ArrayList<>();
         ArrayList<String> entry6 = new ArrayList<>();
+        //Include in each array:
+        // the first name, the position, and the separation date.
+        entry1.add("John");
+        entry1.add("Manager");
+        entry1.add("2016-12-31");
+        entry2.add("Tou");
+        entry2.add("Software Engineer");
+        entry2.add("2016-12-31");
+        entry3.add("Michaela");
+        entry3.add("District Manager");
+        entry3.add("2015-12-19");
+        entry4.add("Jake");
+        entry4.add("Programmer");
+        entry4.add("");
+        entry5.add("Jacquelyn");
+        entry5.add("DBA");
+        entry5.add("");
+        entry6.add("Sally");
+        entry6.add("Web Developer");
+        entry6.add("2015-12-18");
         //Put the last name of each entry as well as what list they belong to into the hashmap.
+        firstMap.put("Johnson", entry1);
+        firstMap.put("Xiong", entry2);
+        firstMap.put("Michaelson", entry3);
+        firstMap.put("Jacobson", entry4);
+        firstMap.put("Jackson", entry5);
+        firstMap.put("Webber", entry6);
         //use a function called sortbyLastName to sort the entries by their last name.
         //display an organized table of each entry organized by last name.
-        System.out.println("Name \t\t\t\t| Postion \t\t\t\t| Separation Date");
-        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("Name \t\t\t\t| Position \t\t\t\t| Separation Date");
+        System.out.println("--------------------|-----------------------|----------------");
         app.sortbyLastName();
     }
 
@@ -37,6 +62,13 @@ public class Solution39 {
         //create a TreeMap called newMap. This map needs a string and an ArrayList of strings.
         TreeMap<String, ArrayList<String>> newMap = new TreeMap<>();
         //within newMap, put all the entries of map into treeMap.
+        newMap.putAll(firstMap);
         //using an enchanced for loop, print out each entry, organized by last name.
+        for(Map.Entry<String, ArrayList<String>> value : newMap.entrySet()){
+            ArrayList <String> temp = value.getValue();
+            System.out.format("%s %s\t| %s\t\t\t\t\t|\t\t%s", temp.get(0), value.getKey(),
+                    temp.get(1), temp.get(2));
+            System.out.println();
+        }
     }
 }
