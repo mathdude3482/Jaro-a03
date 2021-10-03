885066c67a46cea3a2c602f2483ee6b395670a1e
mathdude3482 <orajsamej@gmail.com>
2021-10-03 10:54:07 -0400

    Updated code and JUnit testing.

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -19,15 +19,16 @@ public class Solution31 {
     private static final Scanner in = new Scanner(System.in);
     private static String age;
     private static String heartrate;
+    private static Solution31 app = new Solution31();
     public static void main(String[] args) {
         //Using a while loop, ask for the user's age and heart rate.
         //Enter while loop here. Declare a control value x to be zero.
         int x = 0;
         while(x < 1){
-            age = getInfo("age: ");
-            heartrate = getInfo("resting pulse: ");
+            age = app.getInfo("age:");
+            heartrate = app.getInfo("resting pulse:");
             //check if the user input is valid.
-            if(!(validInput(age) && validInput(heartrate))){
+            if(!(app.validInput(age) && app.validInput(heartrate))){
                 //if invalid, keep asking the user for inputs.
                 System.out.println("Sorry. There's at least one wrong input.");
             }
@@ -37,25 +38,25 @@ public class Solution31 {
             }
         }
         //convert the String inputs into integers.
-        int userAge = getAge(age);
-        int userRHeartRate = getrHeartRate(heartrate);
-            //Display the output by using a Display function, which calls a getHeartRate function.
-        display(userAge, userRHeartRate);
+        int userAge = app.getAge(age);
+        int userRHeartRate = app.getrHeartRate(heartrate);
+        //Display the output by using a Display function, which calls a getHeartRate function.
+        app.display(userAge, userRHeartRate);
     }
     //take in one parameter: the string prompt.
-    private static String getInfo(String prompt) {
+    private String getInfo(String prompt) {
         //print out the prompt.
         System.out.print(prompt);
         //return user input.
         return in.nextLine();
     }
-    public static boolean validInput(String input){
+    public boolean validInput(String input){
         //take in one parameter: the user input.
         //using regex, determine if the input is valid or not.
         boolean determine = true;
         //if there is any whitespace, remove it.
         String revisedinput = input.replaceAll("\\s", "");
-        String regex = "^(?=.*[a-zA-Z])" + "(?=\\S+$).{1,100}$";
+        String regex = "^(?=.*[^0-9])" + "(?=\\S+$).{1,100}$";
         Pattern ratePattern = Pattern.compile(regex);
         Matcher matchrate = ratePattern.matcher(revisedinput);
         //if there is any letter or special character, the input is invalid. Return false.
@@ -66,25 +67,25 @@ public class Solution31 {
         //otherwise, return true.
         return determine;
     }
-    private static int getAge(String age){
+    private int getAge(String age){
         //pass in one parameter: the String age.
         //return the age as an integer.
         return Integer.parseInt(age);
     }
-    private static int getrHeartRate(String rest){
+    private int getrHeartRate(String rest){
         //pass in one parameter: the String resting heart rate.
         //return the hearting rate as an integer.
         return Integer.parseInt(rest);
     }
     //take in three parameters: the resting heart rate, the age, and the intensity percentage
     // as integers.
-    public static double getheartRate(int age, int rest, int intensity) {
+    public double getheartRate(int age, int rest, int intensity) {
         //return the target heart rate, calculated by the Karvonen formula.
         double decimal = intensity / 100.00;
         return Math.round(((220 - age) - rest) * decimal + rest);
     }
     //pass in three parameters: the resting heart rate, the age, and the target heart rate.
-    private static void display(int age, int rest){
+    private void display(int age, int rest){
         //Display the results in a table format.
         //use a for loop to print out the intensity.
         System.out.format("  %s         |  %s", "intensity", "rate");
--- exercise31/src/test/java/baseline/Solution31Test.java
+++ exercise31/src/test/java/baseline/Solution31Test.java
@@ -9,15 +9,17 @@ Solution31 app = new Solution31();
     @Test
     void validInput() {
         String input1 = "1abc";
-        String input2 = "12";
         boolean expected_1 = false;
         boolean actual_1 = app.validInput(input1);
+        assertEquals(expected_1,actual_1);
+    }
+    @Test
+    void validInput2(){
+        String input2 = "12";
         boolean expected_2 = true;
         boolean actual_2 = app.validInput(input2);
-        assertEquals(expected_1,actual_1);
         assertEquals(expected_2, actual_2);
     }
-
     @Test
     void getheartRate() {
         int age = 22;
